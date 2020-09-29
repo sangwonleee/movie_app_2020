2bbc83aa303e8c3024d7b1942b851d1fd9f915f2
sangwonleee <sangwon0959@gmail.com>
2020-09-29 09:22:43 +0900

    Updates

---
+++ precache-manifest.c4393a8e7f86e16aa86a4d9a46343a04.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dfce443a2bf69990919904ed0e82e9a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "b280e50e84e0d838bea5",
+    "url": "/movie_app_2020/static/css/main.fd3a8a9c.chunk.css"
+  },
+  {
+    "revision": "7b8b15f17427b26df90d",
+    "url": "/movie_app_2020/static/js/2.17da08f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.17da08f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b280e50e84e0d838bea5",
+    "url": "/movie_app_2020/static/js/main.4684d32b.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
